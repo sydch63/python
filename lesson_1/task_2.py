f1541279e4dc8bcdb9bc6417635489e3ec2956c6
sydch <sid.pasch@gmail.com>
2022-04-15 17:58:36 +0400

    Выполнена задача №2

--- lesson_1/task_2.py
+++ lesson_1/task_2.py
@@ -4,4 +4,46 @@
 # К каждому элементу списка добавить 17 и заново вычислить сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7.
 # * Решить задачу под пунктом b, не создавая новый список.
 
+tz = 0
+number_1 = tz % 10
+tz = tz // 10
+number_2 = tz % 10
+tz = tz // 10
+
+
+number_range = range(1,1001)
 list_cubed = []
+list_divided_7 = []
+for number in number_range:
+    if number % 2 != 0:
+        number_cubed = number * (number**2)
+        list_cubed.append(number_cubed)
+for number in list_cubed:
+    sum_number = 0
+    number_finally = number
+    while number // 10 != 0:
+        number_remain = number % 10
+        sum_number += number_remain
+        number = number // 10
+        if number // 10 == 0:
+            sum_number += number
+    if sum_number % 7 == 0 and sum_number != 0:
+        list_divided_7.append(number_finally)
+    sum_number_divided_7 = 0
+for number in list_divided_7:
+    sum_number_divided_7 += number
+print("Сумма цифр деленных на 7:", sum_number_divided_7)
+for number in list_cubed:
+    number += 17
+    sum_number = 0
+    number_finally = number
+    while number // 10 != 0:
+        number_remain = number % 10
+        sum_number += number_remain
+        number = number // 10
+        if number // 10 == 0:
+            sum_number += number
+    if sum_number % 7 == 0 and sum_number != 0:
+        list_divided_7.append(number_finally)
+    sum_number_divided_7 += number
+print("Сумма цифр деленных на 7:", sum_number_divided_7)
