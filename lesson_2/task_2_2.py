ce745a3789be56d0a4d4c55127040d068d08a243
sydch <sid.pasch@gmail.com>
2022-04-21 16:23:19 +0400

    Выполнена задача №2

--- lesson_2/task_2_2.py
+++ lesson_2/task_2_2.py
@@ -1,7 +1,7 @@
 test1 = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 test2 = ['примерно в', '23', 'часа', '8', 'минут', '03', 'секунд', 'температура', 'воздуха', 'была', '-5', 'градусов Цельсия', 'темп', 'воды', '+12', 'градусов', 'Цельсия']
 test3 = ['+9', 'примерно в', '23', 'часа', '8', 'минут', '03', '05', 'секунд', 'температура', 'воздуха', 'была', '5', 'градусов Цельсия', 'темп','воды','+2.0','градусов','Цельсия' ,'-2', '11']
-lst1 = test3
+lst1 = test2
 quotation_mark = '"'
 new_lst1 = []
 finally_str = ''
@@ -9,45 +9,33 @@ for data in lst1:
     if data.isdigit() == True or data.startswith("+") or data.startswith("-"):
         if len(data) == 1:
             data_replace = data.replace(data, "0" + data)
+            new_lst1.append(quotation_mark)
             new_lst1.append(data_replace)
-            index_data = new_lst1.index(data_replace)
-            new_lst1.insert(index_data, quotation_mark)
-            index_data += 2
-            new_lst1.insert(index_data, quotation_mark)
+            new_lst1.append(quotation_mark)
         elif data.startswith("+"):
             if len(data) == 2:
                 data_replace = data.replace("+", "+0")
+                new_lst1.append(quotation_mark)
                 new_lst1.append(data_replace)
-                index_data = new_lst1.index(data_replace)
-                new_lst1.insert(index_data, quotation_mark)
-                index_data += 2
-                new_lst1.insert(index_data, quotation_mark)
+                new_lst1.append(quotation_mark)
             else:
+                new_lst1.append(quotation_mark)
                 new_lst1.append(data)
-                index_data = new_lst1.index(data)
-                new_lst1.insert(index_data, quotation_mark)
-                index_data += 2
-                new_lst1.insert(index_data, quotation_mark)
+                new_lst1.append(quotation_mark)
         elif data.startswith("-"):
             if len(data) == 2:
                 data_replace = data.replace("-", "-0")
+                new_lst1.append(quotation_mark)
                 new_lst1.append(data_replace)
-                index_data = new_lst1.index(data_replace)
-                new_lst1.insert(index_data, quotation_mark)
-                index_data += 2
-                new_lst1.insert(index_data, quotation_mark)
+                new_lst1.append(quotation_mark)
             else:
+                new_lst1.append(quotation_mark)
                 new_lst1.append(data)
-                index_data = new_lst1.index(data)
-                new_lst1.insert(index_data, quotation_mark)
-                index_data += 2
-                new_lst1.insert(index_data, quotation_mark)
+                new_lst1.append(quotation_mark)
         else:
+            new_lst1.append(quotation_mark)
             new_lst1.append(data)
-            index_data = new_lst1.index(data)
-            new_lst1.insert(index_data, quotation_mark)
-            index_data += 2
-            new_lst1.insert(index_data, quotation_mark)
+            new_lst1.append(quotation_mark)
     else:
         new_lst1.append(data)
 
@@ -70,17 +58,7 @@ for index,element in enumerate(new_lst1):
     else:
         finally_str += element + ' '
 
-# for index,element in enumerate(new_lst1):
-#     index_next = index + 1
-#     if element == '"' and (new_lst1[index_next].isdigit() == True or new_lst1[index_next].startswith("+") or new_lst1[index_next].startswith("-")):
-#         finally_str += element + ''
-#     elif element.isdigit() == True or element.startswith("+") or element.startswith("-"):
-#         finally_str += element + ''
-#     else:
-#         finally_str += element + ' '
-
-
-print("Окончательная строка: ",finally_str)
+print("Окончательная строка:",finally_str)
 
 
 
