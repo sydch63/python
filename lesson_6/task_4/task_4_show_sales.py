875184d2e0cdb78a65bfca8dc29c74d4b729571b
sydch <sid.pasch@gmail.com>
2022-05-18 23:53:22 +0400

    Выполнено задание №4

--- lesson_6/task_4/task_4_show_sales.py
+++ lesson_6/task_4/task_4_show_sales.py
@@ -6,13 +6,13 @@ with open('bakery.csv','r',encoding='utf-8') as f:
     if len(sys.argv) == 1:
         for el in lst_price:
             print(el)
-    elif len(sys.argv) == 2 and sys.argv[1].isdigit():
+    elif len(sys.argv) == 2 and sys.argv[1].isdigit() and int(sys.argv[1]) <= len(lst_price):
         if type(int(sys.argv[1])) == int:
             for el in lst_price[int(sys.argv[1])-1::]:
                 print(el)
         else:
             print('Некорректный ввод данных')
-    elif len(sys.argv) == 3 and (sys.argv[1].isdigit() and sys.argv[2].isdigit()):
+    elif len(sys.argv) == 3 and (sys.argv[1].isdigit() and sys.argv[2].isdigit()) and int(sys.argv[1]) <= len(lst_price) and int(sys.argv[1]) <= int(sys.argv[2]) :
         if type(int(sys.argv[1])) == int:
             for el in lst_price[int(sys.argv[1])-1:int(sys.argv[2])]:
                 print(el)
