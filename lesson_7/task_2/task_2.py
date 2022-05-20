53d908dd69be20cf801965a98a5e1e5a3e0ba443
sydch <sid.pasch@gmail.com>
2022-05-20 23:11:12 +0400

    Выполнено задание №1,2

---
+++ lesson_7/task_2/task_2.py
@@ -0,0 +1,45 @@
+import os
+import os.path
+
+try:
+    path_conf = os.path.join('.','config_2.yaml')
+    path_conf = os.path.abspath(path_conf)
+    f = open(path_conf,'r')
+
+    lst_conf = []
+
+    for line in f:
+        lst_conf.append(line)
+
+
+
+    for el in lst_conf:
+        if el.startswith(' ') is False and ':' in el:
+            main_path = os.path.join('.',lst_conf[0].replace(':','').strip())
+            main_path = os.path.abspath(main_path)
+        elif el.startswith('  - ') and ':' in el:
+            sec_path = os.path.join(main_path,el.strip().replace('- ','').replace(':',''))
+            sec_path = os.path.abspath(sec_path)
+            os.makedirs(sec_path,exist_ok=True)
+        elif el.startswith('    - ') and ':' in el:
+            third_path = os.path.join(sec_path,el.strip().replace('- ','').replace(':',''))
+            third_path = os.path.abspath(third_path)
+            os.makedirs(third_path,exist_ok=True)
+        elif el.startswith('      - ') and ':' in el:
+            fourth_path = os.path.join(third_path,el.strip().replace('- ','').replace(':',''))
+            fourth_path = os.path.abspath(fourth_path)
+            os.makedirs(fourth_path,exist_ok=True)
+        elif el.startswith('    - ') and '.' in el:
+            file_path = os.path.join(sec_path, el.strip().replace('- ', '').replace(':', ''))
+            file_path = os.path.abspath(file_path)
+            with open(file_path,'w') as f:
+                pass
+        elif el.startswith('        - ') and '.' in el:
+            file_sec_path = os.path.join(fourth_path, el.strip().replace('- ', '').replace(':', ''))
+            file_sec_path = os.path.abspath(file_sec_path)
+            with open(file_sec_path, 'w') as f:
+                pass
+    f.close()
+except (FileNotFoundError,EOFError) as e:
+    print(f'concrete error: {e}')
+
