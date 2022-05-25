f1e0d60899cc4ce5fb2eaf9be07a09c7248da42f
sydch <sid.pasch@gmail.com>
2022-05-26 00:13:27 +0400

    Выполнено задание №2

---
+++ lesson_8/task_8_2.py
@@ -0,0 +1,17 @@
+import re
+
+def parsed_raw(raw_str):
+    try:
+        with open('nginx_logs.txt', 'r', encoding='utf-8') as f:
+            str_file = f.read()
+            parse_ip = re.findall("(\d{1,3}\.\d{1,3}\.+\d{1,3}\.\d{1,3})\s",str_file)
+            print(f"Длина списка всех IP-адресов: {len(parse_ip)}")
+        parse_str = re.findall( "(^\d+\.\d+\.+\d+\.\d+).+\[(\d+\/\w+\/\d+:\d+:\d+:\d+.+)\].+\"(\w+).(\/\w+\/\w+)\s\S+.(\d+).(\d+)", raw_str)
+        if parse_str != []:
+            return parse_str[0]
+    except ValueError as exc:
+        raise ValueError((f"wrong str: {raw_str}"))  from exc
+    except IndexError as exc:
+        raise ValueError((f"wrong str: {raw_str}"))  from exc
+
+print(parsed_raw('144.92.16.161 - - [17/May/2015:12:05:32 +0000] "GET /downloads/product_1 HTTP/1.1" 404 324 "-" "Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.21)"'))
