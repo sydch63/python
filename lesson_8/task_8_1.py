c5720e30a8e0b10c8496f19320017ab62b0f828a
sydch <sid.pasch@gmail.com>
2022-05-30 21:28:42 +0400

    Исправлено задание №1

--- lesson_8/task_8_1.py
+++ lesson_8/task_8_1.py
@@ -3,20 +3,14 @@ import re
 def email_parse(email):
     try:
         dct_email = {}
-        username, domain = email.split('@')
-        with open('task_8_1_test_email.txt','r',encoding='utf-8') as f:
-            str_file = f.readline()
-            while str_file:
-                parse_file = re.findall("(^\w+[\'\.\_\+\-]\w+)@(\w+[\.|\-]\w+\.\w+|\w+[\.|\-]\w+)", str_file)
-                if parse_file != [] and (parse_file[0][0] == username and parse_file[0][1] == domain):
-                    dct_email['username'] = parse_file[0][0]
-                    dct_email['domain'] = parse_file[0][1]
-                    return dct_email
-                else:
-                    str_file = f.readline()
-    except ValueError as exc:
-        raise ValueError((f"wrong email: {email}"))  from exc
-    except IndexError as exc:
-        raise ValueError((f"wrong email: {email}"))  from exc
+        parse_email = re.findall("(^\w+[\'\.\_\+\-]\w+)@(\w+[\.|\-]\w+\.\w+|\w+[\.|\-]\w+)", email)
+        if parse_email != []:
+            dct_email['username'] = parse_email[0][0]
+            dct_email['domain'] = parse_email[0][1]
+            return dct_email
+        else:
+            raise ValueError
+    except ValueError :
+        raise ValueError((f"wrong email: {email}"))
 
-print(email_parse("user14_name@domen-name.ru"))
+print(email_parse("user12_name@domenna.me.ru"))
